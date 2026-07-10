801fac006516aecfc8fbacc9fa792fc3c1247b95
AdjacentAspect <jasonngor11@gmail.com>
2026-07-11 01:51:06 +1000

    v1.0.0

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,12 @@ import App from "./App";
 
 import "./index.css";
 
+import { registerSW } from "virtual:pwa-register";
+
+registerSW();
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
